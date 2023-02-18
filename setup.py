62b81e1e485e58f8b0e331f69e684bca77b52e2d
Your Name <you@example.com>
2023-02-18 08:05:10 -0800

    init commit

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='automataChecker',
     version='1.0.0',
     packages=['automata_checker'],
-    url='',
+    url='https://github.com/qqIdea/automataChecker',
     license='Apache License 2.0',
     author='Alireza',
     description='automata validator',
